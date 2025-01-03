8ffcdead0fa3703d1846e8be19ce9089434af250
tales1982 <142450583+TalesPortfolio@users.noreply.github.com>
2025-01-03 18:40:07 +0100

    New contract more test and feature

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,14 +3,21 @@ require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.24",
+  paths: {
+    sources: "./contracts",
+    artifacts: "./artifacts",
+    cache: "./cache",
+  },
   networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_API_URL, // Substitua pelo link correto da Sepolia no Alchemy ou outro provedor
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111, // Chain ID da rede Sepolia
+    },
     polygonAmoyTestnet: {
       url: process.env.ALCHEMY_API_URL,
-      accounts:
-        process.env.PRIVATE_KEY && process.env.PRIVATE_KEY_TREASURY
-          ? [process.env.PRIVATE_KEY, process.env.PRIVATE_KEY_TREASURY]
-          : [],
-      chainId: 11155111, // Chain ID da Sepolia
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 80002,
     },
   },
 };
